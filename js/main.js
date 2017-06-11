9c7c2d5db074533efd235a450f527535f9724ba7
Bridger <bridgerrhammond@gmail.com>
2017-06-11 17:33:46 -0400

    Did a lot of work since my last commit. I finally figured out how to access everything I wanted from the API's JSON return. It doens't look beautiful but it gets the job done for now!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+/*jslint devel: true, eqeq: true, sloppy: true, vars: true*/
 /*global $, jQuery, alert*/
 
 $(document).ready(function () {
+    lightbox.option({
+      'showImageNumberLabel': false,
+      'maxWidth': 400,
+      'maxHeight': 400
+  });
+    var pokeData = [];
     var arr = []; // Will store the random list of pokemon id #s
     var pokeName = ""; // Had to declare there outside of the function, not sure why to be honest.
+    var pokeTypes = [];
 
     // This took forever to figure out. I wanted unique numbers so that no pokemon is doubled.
     while (arr.length < 12) {
@@ -16,9 +24,21 @@ $(document).ready(function () {
         var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + random;
         var pokeImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + random + ".png";
         $.getJSON(pokeURL, function (data) {
-            var pokeName = data.name;
+            var pokeName = "Name: " + data.name;
+            var pokeWeight = "Weight: " + data.weight;
+            var pokeHeight = "Height: " + data.height;
+            for (i = 0; i < data.types.length; i++) {
+              if (typeof data.types[i].name != "undefined") {
+                pokeTypes.push(data.types[i].name);
+                console.log(data.types[i].name);
+              }
+            };
+            pokeTypes = pokeTypes.join(', ');
+            var pokeType = "Type(s): " + pokeTypes
+            var pokeInfo = pokeName + "</br>" + pokeWeight + "</br>" + pokeHeight + "</br>" + pokeType;
+            pokeTypes = [];
             console.log(pokeName);
-            document.getElementById('main').innerHTML += "<article class='pokeContainer'><img class='pokeImage' src=" + pokeImage + "></article>";
+            document.getElementById('pokeTable').innerHTML += "<a class='pokeContainer gallery-image' href='" + pokeImage + "' data-lightbox='pokeBox' data-title='" + pokeInfo + "'>" + "<img class='pokeImage gallery-thumbnails' src=" + pokeImage + " alt=''></a>";
         });
     });
 });
