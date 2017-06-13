98feb032a54a5360c30cee8cf7a200a78fa65043
Bridger <bridgerrhammond@gmail.com>
2017-06-13 15:26:41 -0400

    Sync

--- js/main.js
+++ js/main.js
@@ -31,14 +31,15 @@ $(document).ready(function () {
             for (i = 0; i < data.types.length; i++) {
                 if (typeof data.types[i].name != "undefined") {
                     pokeTypes.push(data.types[i].name);
-                    // console.log(data.types[i].name); // Any console.log() you see is for my own debugging and practice during coding :)
+                    // console.log(data.types[i].name); 
+                    // Any console.log() you see is for my own debugging and practice during coding
                 }
             };
             pokeTypes = pokeTypes.join(', ');
             var pokeType = "Type(s): " + pokeTypes
             var pokeInfo = pokeName + "</br>" + pokeWeight + "</br>" + pokeHeight + "</br>" + pokeType;
             pokeTypes = [];
-            // console.log(pokeName); // Any console.log() you see is for my own debugging and practice during coding :)
+            // console.log(pokeName);
             document.getElementById('pokeTable').innerHTML += "<li><a class='pokeContainer gallery-image' href='" + pokeImage + "' data-lightbox='pokeBox' data-title='" + pokeInfo + "'>" + "<img class='pokeImage gallery-thumbnails' src=" + pokeImage + " alt=''></a></li>";
         });
     });
